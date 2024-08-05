59b5970c6fc24cf63b505a31a8d896caf7cff88e
Joao Pedro <joaopedro.jusus@gmail.com>
2024-08-05 19:09:20 -0300

    Subindo hook use-State

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
-import Home from "./paginas/home/Home";
-import "./App.css";
+import Home from './paginas/home/Home';
+import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-
   const [valor, setValor] = useState(0);
 
   function handleClick() {
     setValor(valor + 1);
   }
-
   return (
-    <Home></Home>
-
+    //  <Home  title="Componente Home"
+    //         description="Este é um componente Home que recebe props."/>
+    <div>
+      <p>O valor é: {valor}</p>
+      <button onClick={handleClick}>Adicionar 1</button>
+    </div>
   );
 }
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,25 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from 'react';
 import './Home.css';
 
-function Home() {
-
-    const [completed, setCompleted] = useState(false);
-    const [tarefa, setTarefa] = useState('');
-
-    useEffect(() => {
-        if(completed){ 
-            setTarefa('Parabéns! Você concluiu a tarefa!');
-        }
-    }, [completed]);
-    
-  return (
-    <div>
-        <h1>Tarefa</h1>
-        <h3>{tarefa}</h3>
-        <p>Conclua a tarefa</p>
-        <button onClick={() => setCompleted(true)}>Concluir Tarefa</button>
-    </div>
-  )
+interface minhaProps {
+    title: string;
+    description: string;
+  }
+  
+function Home(props:minhaProps){
+    return (
+        <>
+        <h2>{props.title}</h2>
+        <p>{props.description}</p>
+        </>
+    );
 }
 
 export default Home;
