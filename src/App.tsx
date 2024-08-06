adf4c274dd561cd2807f386817bacc57091bad1f
Joao Pedro <joaopedro.jusus@gmail.com>
2024-08-06 18:22:57 -0300

    Criando meu footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import Home from './paginas/home/Home'
+import React from "react";
+import Home from "./paginas/home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./paginas/home/Login";
 
 function App() {
   return (
-    < Home />
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
